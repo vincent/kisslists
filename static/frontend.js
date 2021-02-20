a6457607e618bc3ac77092a870a80f2c4a276c52
Jacob Adlers <jacob.adlers@gmail.com>
2021-02-20 15:26:48 +0100

    Use SVG for delete icon
    Taken from https://feathericons.com

--- static/frontend.js
+++ static/frontend.js
@@ -200,7 +200,10 @@
                     <use xlink:href="#todo__circle" class="todo__circle"></use>
                 </svg>
                 <div class="todo__text">${item.text}</div>
-                <div class="todo__delete">╳</div>
+                <svg class="todo__delete" viewBox="0 0 24 24" width="24" height="24" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1">
+                    <line x1="18" y1="6" x2="6" y2="18"></line>
+                    <line x1="6" y1="6" x2="18" y2="18"></line>
+                </svg>
             </label>`;
         },
     
